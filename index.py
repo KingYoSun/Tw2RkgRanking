202c548ee66121c93de93a827d39e1a8138bc83c
KingYoSun <nocking257838@gmail.com>
2020-03-01 08:23:42 +0000

    Update tweet status

--- index.py
+++ index.py
@@ -6,7 +6,6 @@ import json
 import tweepy
 import datetime
 from boto3.dynamodb.conditions import Key
-from decimal import Decimal, ROUND_DOWN
 
 #自作関数
 import functions
@@ -30,12 +29,13 @@ finally:
     print('Finish AWS Setup')
 
 #ランキングのアイテム数
-max_item = 100
+max_item = 80
+#現在時刻
+updated_at = functions.get_update_at()
 
 class DynamoDBTweet:
     def __init__(self):
         self.ranker = []
-        self.load_count = 0
     
     def get_ranker(self):
         try:
@@ -49,20 +49,18 @@ class DynamoDBTweet:
         else:
             self.ranker = ranker["Items"]
     
-    def get_tweet(self, maxItems):
-        updated_at = functions.get_update_at() 
+    def get_tweet(self):
         today = updated_at["updated_at_date"]
-        last_day = today - Decimal(60*60*24)
-        self.load_count = maxItems - len(self.ranker)
+        last_day = today - functions.return_decimal(60*60*24)
         try:
             tweet_list_today = table.query(
                 KeyConditionExpression = Key('updated_at_date').eq(today),
                 ScanIndexForward = False,
-                Limit = self.load_count
+                Limit = max_item
             )
             tweet_list = tweet_list_today["Items"]
-            if tweet_list_today["Count"] < self.load_count:
-                additional_load_count = self.load_count - tweet_list_today["Count"]
+            if tweet_list_today["Count"] < max_item:
+                additional_load_count = max_item - tweet_list_today["Count"]
                 tweet_list_last_day = table.query(
                     KeyConditionExpression = Key('updated_at_date').eq(last_day),
                     ScanIndexForward = False,
@@ -78,13 +76,10 @@ class DynamoDBTweet:
                 d_RT = tweet_list[i].get("d_RT", 0)
                 tweet_list[i]["rate"] = functions.get_rate(d_fav, d_RT)
             self.ranker.extend(tweet_list)
-    
-    def sort_ranker(self):
-        self.ranker = sorted(self.ranker, key=lambda x:x["rate"], reverse=True)
 
-class GetTweet:
-    def __init__(self):
-        self.tweet_data = []
+class UpdateTweet:
+    def __init__(self, ranker):
+        self.ranker = ranker
         self.set_twitter_api()
     
     #Twitterオブジェクトの生成
@@ -98,26 +93,42 @@ class GetTweet:
         finally:
             print('Set Twitter API Object')
     
-    #単一ツイートの取得
-    def get_tweet_status(self, id):
-        try:
-            result = self.api.get_status(id)
-        except Exception as e:
-            raise("get_status Error: " + str(e))
-        else:
-            return result
+    def sort_ranker(self):
+        self.ranker = sorted(self.ranker, key=lambda x:x["rate"], reverse=True)
     
-
+    #単一ツイートの取得
+    def get_tweet_status(self):
+        for i in range(len(self.ranker)):
+            try:
+                result = self.api.get_status(self.ranker[i]["id"])
+            except Exception as e:
+                raise("get_status Error: " + str(e))
+            else:
+                self.ranker[i]["favorite"] = functions.return_decimal(result.favorite_count)
+                self.ranker[i]["retweet"] = functions.return_decimal(result.retweet_count)
+                #最後にupdateされてからの時間
+                time_now = updated_at["updated_at_date"] + updated_at["updated_at_time"]
+                at_last_updated = self.ranker[i]["updated_at_date"] + self.ranker[i]["updated_at_time"]
+                time_last_update = time_now - at_last_updated
+                #time_last_updateが一時間(60*60 = 3600)以上の場合、rateの再計算
+                if time_last_update > 3600:
+                    self.ranker[i]["d_fav"] = self.ranker[i]["favorite"] - self.ranker[i]["past_favorite"]
+                    self.ranker[i]["past_favorite"] = self.ranker[i]["favorite"]
+                    self.ranker[i]["d_RT"] = self.ranker[i]["retweet"] - self.ranker[i]["past_retweet"]
+                    self.ranker[i]["past_retweet"] = self.ranker[i]["retweet"]
+                    self.ranker[i]["rate"] = functions.get_rate(self.ranker[i]["d_fav"], self.ranker[i]["d_RT"])
+                
 def handler(event, context):
     dynamoDB_tweet = DynamoDBTweet()
     dynamoDB_tweet.get_ranker()
-    #RankingDBリセット時
-    if len(dynamoDB_tweet.ranker) < max_item:
-        dynamoDB_tweet.get_tweet(max_item)
-    dynamoDB_tweet.sort_ranker()
+    dynamoDB_tweet.get_tweet()
+    update_tweet = UpdateTweet(dynamoDB_tweet.ranker)
+    update_tweet.get_tweet_status()
+    update_tweet.sort_ranker()
+    print("count: {}".format(len(update_tweet.ranker)))
     return{
         'isBase64Encoded': False,
         'statusCode': 200,
         'headers': {},
-        'body': dynamoDB_tweet.ranker
+        'body':update_tweet.ranker
     }
